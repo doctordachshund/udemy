7ad228b77c90fca15ca53487dca96c2743f3d5c4
Matthew Adams <matthewdadams92@gmail.com>
2018-07-25 11:38:05 -0400

    finished client side authentication

--- stephen-grider/advserver/index.js
+++ stephen-grider/advserver/index.js
@@ -5,6 +5,7 @@ const morgan = require("morgan");
 const app = express();
 const router = require("./router");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 // DB Setup
 // mongoose.connect("mongodb://localhost:auth/auth");
@@ -16,6 +17,7 @@ mongoose.connect(
 
 // App Setup
 app.use(morgan("combined"));
+app.use(cors());
 app.use(bodyParser.json({ type: "*/*" }));
 router(app);
 
